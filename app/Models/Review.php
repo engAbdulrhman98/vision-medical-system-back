60223bc28ec03477c12f80507fa4e116872f0ed9
abdulrhman <ewwe1198@gmail.com>
2026-07-24 19:36:08 +0300

    Explicitly define fillable property on Review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     protected $guarded = [];
+    protected $fillable = ['product_id', 'user_name', 'reviewer_name', 'rating', 'comment', 'approved'];
 
     public function product()
     {
